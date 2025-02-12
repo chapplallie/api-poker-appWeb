ab22dc9cf00446871edf5a4b67d3d1130bb14272
AntoineCor35 <antoine.cormier@my-digital-school.org>
2025-02-12 16:46:38 +0100

    updated app module with new services: game-logic, players, decks, and players-actions

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { TablesController } from './tables/tables.controller';
 import { TablesService } from './tables/tables.service';
 import { ActionsController } from './actions/actions.controller';
 import { ActionsService } from './actions/actions.service';
+import { GameLogicService } from './game-logic/game-logic.service';
+import { PlayersService } from './players/players.service';
+import { DecksService } from './decks/decks.service';
+import { PlayersActionsService } from './players-actions/players-actions.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TablesController, ActionsController],
-  providers: [AppService, TablesService, ActionsService],
+  providers: [AppService, TablesService, ActionsService, GameLogicService, PlayersService, DecksService, PlayersActionsService],
 })
 export class AppModule {}
+
