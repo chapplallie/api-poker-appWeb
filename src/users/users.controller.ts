e0671672d49e6bcbf85a71e2d55895d37794d906
Lallie.chappon <chapplallie@gmail.fr>
2025-05-14 12:10:14 +0200

    add : access to user data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UsePipes, ValidationPipe, UseGuards, NotFoundException, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Public } from 'src/auth/decorators/public';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -12,7 +13,21 @@ export class UsersController {
       return this.usersService.getAllUsers();
   }
 
-  
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  async getProfile(@Req() req: any) {
+    const userId = req.user.id; // Extract the user ID from the JWT payload
+    const user = await this.usersService.getUserById(userId); // Fetch the full user data from UsersService
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    // Exclude sensitive fields like the password
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+  }
+
   @Get(':id')
   getUserById(@Param('id') id: number): any {
     return this.usersService.getUserById(id);
